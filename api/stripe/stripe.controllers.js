d99ad82d80cc8f937052a54afa8dff7ba9b1c4bc
adrianvilla1504 <adriancvilla@gmail.com>
2022-10-15 09:21:48 -0500

    feat: fix same email send multiple times

--- api/stripe/stripe.controllers.js
+++ api/stripe/stripe.controllers.js
@@ -4,58 +4,66 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  async function paymentTry(req, res) {
+async function paymentTry(req, res) {
+  const { profile, items } = req.body;
+  const cemail = profile.email;
 
-    const { profile, items } = req.body;
-    const cemail = profile.email;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    receipt_email: cemail,
+  });
 
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculateOrderAmount(items),
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-      receipt_email: cemail,
-    });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+}
 
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  }
+const receiptemail = async (req) => {
+  const { profile, cart } = req.body;
+  let controllerSent = 0;
 
-  const receiptemail = async (req) => {
-    const { profile, cart } = req.body;
-    let totalCost = 0;
-    cart?.forEach((product)=>{
-      totalCost += product.price*product.qty
-    })
+  if (controllerSent === 0 && profile) {
+    controllerSent = 1;
+    if (controllerSent === 1) {
+      let totalCost = 0;
+      cart?.forEach((product) => {
+        totalCost += product.price * product.qty;
+      });
 
-    console.log("profile receipt", profile);
-    console.log("Products receipt", cart);
-    console.log("Total to pay:", totalCost)
+      console.log("profile receipt", profile);
+      console.log("Products receipt", cart);
+      console.log("Total to pay:", totalCost);
 
-    const msg = {
-      to: profile.email, // Change to your recipient
-      from: "adriancvilla@gmail.com", // Change to your verified sender
-      subject: `Here is your order receipt ${profile.name}`,
-      template_id: 'd-6e4e0084952946fabae7b74c4077bb3a',
-      dynamic_template_data: {
-        clientname: profile.name,
-        phone: profile.phone,
-        receiptItems: {item: cart},
-        totalcost: totalCost,
+      const msg = {
+        to: profile.email, // Change to your recipient
+        from: "adriancvilla@gmail.com", // Change to your verified sender
+        subject: `Here is your order receipt ${profile.name}`,
+        template_id: "d-6e4e0084952946fabae7b74c4077bb3a",
+        dynamic_template_data: {
+          clientname: profile.name,
+          phone: profile.phone,
+          receiptItems: { item: cart },
+          totalcost: totalCost,
+        },
+      };
+
+      try {
+        await sgMail.send(msg);
+        console.log("The e-mail has been sent to the client");
+      } catch (error) {
+        console.log(error);
       }
-    };
-
-    try {
-      await sgMail.send(msg);
-      console.log("The e-mail has been sent to the client");
-    } catch(error) {
-      console.log(error);
     }
+  } else {
+    controllerSent = 0;
   }
 
+};
 
 /* async function invoicing(req, res)
 {
@@ -140,4 +148,4 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   await stripe.invoices.sendInvoice(invoice.id);
 }; */
 
-module.exports = {paymentTry, receiptemail} ;
+module.exports = { paymentTry, receiptemail };
